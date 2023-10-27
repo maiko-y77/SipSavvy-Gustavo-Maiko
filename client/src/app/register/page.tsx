b9427aadde14d3f6e358117cb5fd23b58880249f
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-10-26 17:18:06 -0700

    Added Nabvar and Footter function

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,10 +1,12 @@
 //SIGN UP PAGE
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Register() {
     return (
         <section>
             <Navbar />
+            <Footer />
         </section>
     )
 }
