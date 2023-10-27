a5be4fd9b00c3fdf044f5a826556732341d2867c
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-10-26 17:16:11 -0700

    Added Navbar function

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+    return (
+        <header className="header">
+            <div>
+                <a href="/"><img className="header-navbar-logo" src="" alt="Header / Navbar / Logo" /></a>
+            </div>
+            <nav className="navbar">
+                <a href="/about">About Us</a>
+                <a href="/write">Write</a>
+                <a href="/contact">Contact Us</a>
+                <a href="/login">Sign In</a>
+                <div>
+                    <a href="/register">Sign Up</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
