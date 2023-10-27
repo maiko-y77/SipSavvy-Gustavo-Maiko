d7dfa48ca342ee2e0d8c9d40bdbf60de13eaf045
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-10-26 17:17:27 -0700

    Added Nabvar and Footter function

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -1,10 +1,12 @@
 //CONTACT US PAGE
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Register() {
     return (
         <section>
             <Navbar />
+            <Footer />
         </section>
     )
 }
