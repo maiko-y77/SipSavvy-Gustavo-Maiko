0b60612fa8a6db367e06bca67c3b8746d1d11eab
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-11-01 17:01:33 -0700

    Added logo for admin navbar

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 const logoUrl = '/images/SipSavvy-Logo.svg'
+const adminLogoUrl = '/images/AdminSipSavvy-Logo.svg'
 
-export default function Logo() {
+export function Logo() {
     return (
         <a href="/">
             <Image src={logoUrl} width={163.67} height={64} alt="SipSavvy Logo" />
+        </a>
+    )
+}
+
+export function AdminLogo() {
+    return (
+        <a href="/">
+            <Image src={adminLogoUrl} width={163.67} height={64} alt="SipSavvy Logo" />
         </a>
     )
 }
