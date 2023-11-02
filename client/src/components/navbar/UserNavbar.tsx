c438efdb7a9fbb7b23214f187ee13e84c33099eb
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-01 19:23:04 -0700

    UserNavbar updated

--- client/src/components/navbar/UserNavbar.tsx
+++ client/src/components/navbar/UserNavbar.tsx
@@ -1,33 +1,39 @@
-import { BellIcon, EnvelopeIcon, UserIcon, PowerIcon } from '@heroicons/react/24/outline'
+import {
+  BellIcon,
+  EnvelopeIcon,
+  UserIcon,
+  PowerIcon,
+} from "@heroicons/react/24/outline";
 
 const UserNavbar = () => {
-    return (
-        <nav className="user-writer-admin-navbar">
-            <div className="wrap-links">
-                <a href="/">Home</a>
-                <a href="/collections">My Collections</a>
-                <a href="/contact">Contact Us</a>
-            </div>
-            <div className="wrap-icons">
-                <a href="/" className='icon'>
-                    <BellIcon width={24} height={24}  />
-                </a>
-                <a href="/" className='icon'>
-                    <EnvelopeIcon width={24} height={24}  />
-                </a>
-            </div>
-
-            <div className="droppeddown-loggedin">
-                <a className="avatar icon" href="/" >
-                    <UserIcon width={24} height={24} />
-                </a>
-                <p>Hello, <span>User!</span></p>
-            </div>
-            <a href="/" className='icon'>
-                    <PowerIcon width={22} height={22} />
-                </a>
-        </nav>
-    )
-}
+  return (
+    <nav className="user-writer-admin-navbar">
+      <div className="wrap-links">
+        <a href="/">Home</a>
+        <a href="/collections">My Collections</a>
+        <a href="/contact">Contact Us</a>
+      </div>
+      <div className="wrap-icons">
+        <a href="/" className="icon">
+          <BellIcon width={24} height={24} />
+        </a>
+        <a href="/" className="icon">
+          <EnvelopeIcon width={24} height={24} />
+        </a>
+      </div>
+      <div className="droppeddown-loggedin">
+        <a className="avatar icon" href="/">
+          <UserIcon width={24} height={24} />
+        </a>
+        <p>
+          Hello, <span>User!</span>
+        </p>
+      </div>
+      <a href="/" className="icon">
+        <PowerIcon width={22} height={22} />
+      </a>
+    </nav>
+  );
+};
 
 export default UserNavbar;
--- client/src/components/navbar/WriterNavbar.tsx
+++ client/src/components/navbar/WriterNavbar.tsx
@@ -1,24 +1,34 @@
 const WriterNavbar = () => {
-    return (
-        <nav className="user-writer-admin-navbar">
-            <div className="wrap-links">
-                <a href="/">Home</a>
-                <a href="/articles">My Articles</a>
-                <a href="/Profile">My Profile</a>
-                <a href="/collections">My Collections</a>
-                <a href="/contact">Contact Us</a>
-            </div>
-            <div className="wrap-icons">
-                <a href="/"><img src="#" alt="bell" /></a>
-                <a href="/"><img src="#" alt="vector" /></a>
-            </div>
-            <div className="droppeddown-loggedin">
-                <a className="user" href="/"><img src="#" alt="user" /></a>
-                <p>Hello, <span>Writer!</span></p>
-                <a href="/"><img src="#" alt="power-off" /></a>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="user-writer-admin-navbar">
+      <div className="wrap-links">
+        <a href="/">Home</a>
+        <a href="/articles">My Articles</a>
+        <a href="/Profile">My Profile</a>
+        <a href="/collections">My Collections</a>
+        <a href="/contact">Contact Us</a>
+      </div>
+      <div className="wrap-icons">
+        <a href="/">
+          <img src="#" alt="bell" />
+        </a>
+        <a href="/">
+          <img src="#" alt="vector" />
+        </a>
+      </div>
+      <div className="droppeddown-loggedin">
+        <a className="user" href="/">
+          <img src="#" alt="user" />
+        </a>
+        <p>
+          Hello, <span>Writer!</span>
+        </p>
+        <a href="/">
+          <img src="#" alt="power-off" />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default WriterNavbar;
