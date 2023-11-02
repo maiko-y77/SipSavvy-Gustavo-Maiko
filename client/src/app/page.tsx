44b03487639bb51b7ad4e1f47325d4e2f21c6060
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-11-01 17:03:23 -0700

    Just changed it for switch each logged in page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <>
-      <Navbar type="user"  />
+      <Navbar type="admin" />
       <Footer />
     </>
   )
