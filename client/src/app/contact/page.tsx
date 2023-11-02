8e4623419200b3d04ec92ac820d739b665278002
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-02 16:31:16 -0700

    contact page developed

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -3,7 +3,31 @@
 export default function Register() {
     return (
         <>
-            This is the contact page content
+        <div className="contact full-width">
+           <div className="copy">
+            <h1>Sip, Savor, and Reach Out!</h1>
+            <p>Whether you have a pressing wine question or just want to chat about your latest wine adventure, we&apos;re all ears (and taste buds)!</p>
+            <p>
+            Contact us to share your thoughts, inquiries, or simply to say cheers. 
+We&apos;re here to uncork conversations, one message at a time. Let&apos;s toast to great wine talk!</p>
+           </div>
+
+           <form className="contact-form">
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" required />
+                </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" required />
+                </div>
+                <div className="form-group">
+                    <label>Message</label>
+                    <textarea></textarea>
+                </div>
+                <button type="submit">Send</button>
+           </form>
+           </div>
         </>
     )
 }
