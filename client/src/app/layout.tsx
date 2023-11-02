e3cba20eae55d655ca410240956674cb0cded02a
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-01 19:55:12 -0700

    navbar style wrapped into navbar.scss

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-      <Navbar type="admin" />
+      <Navbar type="user" />
       <main>
         {children}
         </main>
