36c65e5a686ef30eaf7353d6e0a295c773164489
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-03 12:50:24 -0700

    dialog component added and css done

--- client/src/app/first-login/page.tsx
+++ client/src/app/first-login/page.tsx
@@ -3,7 +3,14 @@ import Dialog from "@/components/Dialog";
 const page = () => {
   return (
     <Dialog>
-        Hello
+        <h1>Your wine journey begins here.</h1>
+        <p>But first, we would love to know your name.</p>
+        <form>
+          <div className="form-group">
+            <input type="text" placeholder="Your name here" name="name" />
+          </div>
+          <button type="submit">Let&#39;s Begin!</button>
+        </form>
     </Dialog>
   );
 };
