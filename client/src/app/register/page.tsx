41ce95d88ecd51232b2072f9c41b1037d32f6758
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-03 11:19:08 -0700

    register-page-dev

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,11 +1,34 @@
 //SIGN UP PAGE
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 
 export default function Register() {
     return (
         <>
-            This is the register page content
+            <div className="register-content">
+
+                <h1>Come learn and share with the hippest wine community around!</h1>
+                <p>Enter your email address and password to create an account.</p>
+
+                <form className="register-form">
+                    <div className="form-group">
+                        <label>Your email</label>
+                        <input name="email" type="email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Your Password</label>
+                        <input name="password" type="password" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Confirm your Password</label>
+                        <input name="password" type="password" />
+                    </div>
+
+                    <button>Sign Up</button>
+
+                </form>
+
+            </div>
         </>
     )
 }
