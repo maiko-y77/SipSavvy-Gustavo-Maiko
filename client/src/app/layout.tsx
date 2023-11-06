aa9fd93156879e90820e61f26f04ab408a2e9bc1
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-06 15:54:39 -0800

    modal prop added to layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -10,8 +10,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  modal: React.ReactNode,
 }) {
   return (
     <html lang="en">
@@ -19,6 +21,7 @@ export default function RootLayout({
       <Navbar type="default" />
       <main>
         {children}
+        {modal}
         </main>
       <Footer />
       </body>
