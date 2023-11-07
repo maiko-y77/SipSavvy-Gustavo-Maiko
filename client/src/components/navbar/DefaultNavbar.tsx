96270b960a080b5149407222d6b763e5dfa34360
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-06 16:45:15 -0800

    modal working

--- client/src/components/navbar/DefaultNavbar.tsx
+++ client/src/components/navbar/DefaultNavbar.tsx
@@ -1,14 +1,20 @@
+"use client";
 import Link from "next/link";
 
 export default function DefaultNavbar() {
-    return (
-        <nav className="default_navbar">
-            <Link href="/about">About Us</Link>
-            <Link href="/contact">Contact Us</Link>
-            <Link href="/login">Sign In</Link>
-            <div>
-                <Link href="/register">Sign Up</Link>
-            </div>
-        </nav>
-    )
+  const location = window.location.pathname;
+  return (
+    <nav className="default_navbar">
+      <Link href="/about">About Us</Link>
+      <Link href="/contact">Contact Us</Link>
+      {location === "/login" ? "" : <Link href="/login">Sign In</Link>}
+      {location === "/register" ? (
+        ""
+      ) : (
+        <div>
+          <Link href="/register">Sign Up</Link>
+        </div>
+      )}
+    </nav>
+  );
 }
