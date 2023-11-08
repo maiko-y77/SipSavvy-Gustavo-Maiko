74a83ab122c67273ae1bd5f7b70bf0eacf36e261
Maiko Yorino <mk0510@mk@hotmail.co.jp>
2023-11-09 08:04:01 +0900

    Updated collection page with css

---
+++ client/src/components/CollectionItem.tsx
@@ -0,0 +1,31 @@
+import { BookmarkIcon, TrashIcon, EyeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import AuthorBullet from "./AuthorBullet";
+
+const CollectionItem = () => {
+    return (
+        <div className="collection-item">
+            <div className="collection-details">
+                <div className="collection-div">
+                    <h3 className="collection-title"><Link href="#">Grape Varieties Guides</Link></h3>
+                    <div className="collection-text">12 Articles</div>
+                </div>
+                <div className="collection-options">
+                    <div className="collection-div">
+                        <Link href="#" className="icon-link">
+                            <PencilSquareIcon width={27} height={24} />
+                        </Link>
+                        <Link href="#" className="icon-link">
+                            <EyeIcon width={27} height={24} />
+                        </Link>
+                        <Link href="#" className="icon-link">
+                            <TrashIcon width={27} height={24} />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CollectionItem;
