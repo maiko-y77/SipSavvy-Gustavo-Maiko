188833a93a71b48fa7a39290f13abcbe3f9c6941
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-10 15:06:25 -0800

    mixins file name changed to _mixins typography incorporated into _variables.scss file colors.scss changed to _variables.scss globals.scss updated accordling Footer.scss file created

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import "./Footer.scss";
+
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <p>2023 – Developed by Gustavo Neri & Maiko Yorino</p>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <p>2023 – Developed by Gustavo Neri & Maiko Yorino</p>
+    </footer>
+  );
 }
