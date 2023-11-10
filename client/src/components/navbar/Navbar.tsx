62806abb1f2127c0583104eea36eb60a9a93ec5e
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-10 15:10:28 -0800

    Navbar.tsx moved to components/Navbar Navbar.scss imported into Navbar.tsx Navbar.scss moved from /styles to /components/Navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,43 +1,41 @@
 import { Logo } from "../Logo";
 import { AdminLogo } from "../Logo";
+import "./Navbar.scss";
 import AdminNavbar from "./AdminNavbar";
 import WriterNavbar from "./WriterNavbar";
 import UserNavbar from "./UserNavbar";
 import DefaultNavbar from "./DefaultNavbar";
 
 interface NavbarProps {
-    type: "admin" | "writer" | "user" | "default";
+  type: "admin" | "writer" | "user" | "default";
 }
 
 export default function Navbar({ type }: NavbarProps) {
+  let componentToRender;
 
-    let componentToRender
+  switch (type) {
+    case "admin":
+      componentToRender = <AdminNavbar />;
+      break;
+    case "writer":
+      componentToRender = <WriterNavbar />;
+      break;
+    case "user":
+      componentToRender = <UserNavbar />;
+      break;
+    case "default":
+    default:
+      componentToRender = <DefaultNavbar />;
+      break;
+  }
 
-    switch (type) {
-        case "admin":
-            componentToRender = <AdminNavbar />;
-            break;
-        case "writer":
-            componentToRender = <WriterNavbar />;
-            break;
-        case "user":
-            componentToRender = <UserNavbar />;
-            break;
-        case "default":
-        default:
-            componentToRender = <DefaultNavbar />;
-            break;
-    }
+  // Define the className based on the type
+  const headerClassName = type === "admin" ? "admin-header" : "header";
 
-    // Define the className based on the type
-    const headerClassName = type === "admin" ? "admin-header" : "header";
-
-    return (
-        <header className={headerClassName}>
-            <div>
-                {type === "admin" ? <AdminLogo /> : <Logo />}
-            </div>
-            {componentToRender}
-        </header>
-    )
+  return (
+    <header className={headerClassName}>
+      <div>{type === "admin" ? <AdminLogo /> : <Logo />}</div>
+      {componentToRender}
+    </header>
+  );
 }
