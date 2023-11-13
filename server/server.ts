99b531da355a797063b4ce723693b566550e612b
Maiko Yorino <mk0510@mk@hotmail.co.jp>
2023-11-13 16:16:01 +0900

    Changed from js to ts

---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import articleRoutes from './routes/articleRoutes';
+
+const prisma = new PrismaClient();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/articles', articleRoutes);
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('GET request to the homepage');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
