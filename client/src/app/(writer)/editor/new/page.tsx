5b7a111c720423ab4aaec6117aa7f45af9ba83b2
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-16 13:43:10 -0500

    actions bar design done

--- client/src/app/(writer)/editor/new/page.tsx
+++ client/src/app/(writer)/editor/new/page.tsx
@@ -1,12 +1,36 @@
+import { LinkIcon, PhotoIcon, VideoCameraIcon } from "@heroicons/react/24/outline";
 import styles from "./newArticle.module.scss";
 
 const NewArticle = () => {
   return (
     <div className={styles.container}>
       <form>
-        <div className="options-bar">Options bar</div>
+        <div className={styles.actionsBar}>
+          <div className={styles.wrapper}>
+            <p className={styles.textContainer}>
+              Auto-save is always on, don&apos;t worry!
+            </p>
+            <div className={styles.insertBar}>
+              <p>Insert: </p>
+              <div className={styles.icon}>
+                <PhotoIcon />
+              </div>
+              <div className={styles.icon}>
+                <VideoCameraIcon />
+              </div>
+              <div className={styles.icon}>
+                <LinkIcon />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="editor-container">
-            <input type="text"/>
+          <input type="text" name="article-title" placeholder="Title" />
+          <input
+            type="text"
+            name="article-content"
+            placeholder="What do you want to talk about today?"
+          />
         </div>
         <button>Save Draft</button>
         <button>Publish</button>
