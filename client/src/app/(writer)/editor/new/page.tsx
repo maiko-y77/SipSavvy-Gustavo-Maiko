bb8092634eadab2e977db6e14d72c41a8ab25ef4
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-17 12:24:40 -0500

    TipTap editor installed and implemented

--- client/src/app/(writer)/editor/new/page.tsx
+++ client/src/app/(writer)/editor/new/page.tsx
@@ -1,51 +1,7 @@
-import {
-  LinkIcon,
-  PhotoIcon,
-  VideoCameraIcon,
-} from "@heroicons/react/24/outline";
-import styles from "./newArticle.module.scss";
+import Editor from "@/components/Editor/Editor";
 
 const NewArticle = () => {
-  return (
-    <div className={styles.container}>
-      <form>
-        <div className={styles.actionsBar}>
-          <div className={styles.wrapper}>
-            <p className={styles.textContainer}>
-              Auto-save is always on, don&apos;t worry!
-            </p>
-            <div className={styles.insertBar}>
-              <p>Insert: </p>
-              <div className={styles.icon}>
-                <PhotoIcon />
-              </div>
-              <div className={styles.icon}>
-                <VideoCameraIcon />
-              </div>
-              <div className={styles.icon}>
-                <LinkIcon />
-              </div>
-            </div>
-            <div className={styles.buttons}><button className={styles.saveDraftBtn}>Save Draft</button>
-            <button>Publish</button></div>
-          </div>
-        </div>
-        <div className={styles.editorContainer}>
-          <input
-            className={styles.articleTitle}
-            type="text"
-            name="article-title"
-            placeholder="Title"
-          />
-          <textarea
-            className={styles.articleContent}
-            name="article-content"
-            placeholder="What do you want to talk about today?"
-          ></textarea>
-        </div>
-      </form>
-    </div>
-  );
+  return <Editor />;
 };
 
 export default NewArticle;
---
+++ client/src/components/Editor/Editor.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import styles from "./Editor.module.scss";
+import { PhotoIcon, VideoCameraIcon, LinkIcon } from "@heroicons/react/24/outline";
+
+const ArticleEditor = () => {
+  const editor = useEditor({
+    extensions: [Document, Paragraph, Text],
+    content: "<p>What do you want to talk about today?</p>",
+    autofocus: true,
+    editable: true,
+    injectCSS: false,
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.actionsBar}>
+      <div className={styles.wrapper}>
+            <p className={styles.textContainer}>
+              Auto-save is always on, don&apos;t worry!
+            </p>
+            <div className={styles.insertBar}>
+              <p>Insert: </p>
+              <div className={styles.icon}>
+                <PhotoIcon />
+              </div>
+              <div className={styles.icon}>
+                <VideoCameraIcon />
+              </div>
+              <div className={styles.icon}>
+                <LinkIcon />
+              </div>
+            </div>
+            <div className={styles.buttons}><button className={styles.saveDraftBtn}>Save Draft</button>
+            <button>Publish</button></div>
+          </div>
+      </div>
+      <input name="article_title" className={styles.articleTitle} placeholder="Title"/>
+      <EditorContent className={styles.editorContainer} editor={editor} />
+    </div>
+  );
+};
+
+export default ArticleEditor;
