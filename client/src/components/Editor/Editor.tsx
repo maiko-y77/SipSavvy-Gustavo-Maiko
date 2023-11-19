c2f384cf291556dd788baa170713212c0bb5161e
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-19 12:27:40 -0500

    starting the editor again

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -8,38 +8,37 @@ import {
 
 const ArticleEditor = () => {
   return (
-    <form>
-      <div className={styles.container}>
-        <div className={styles.actionsBar}>
-          <div className={styles.wrapper}>
-            <p className={styles.textContainer}>
-              Auto-save is always on, don&apos;t worry!
-            </p>
-            <div className={styles.insertBar}>
-              <p>Insert: </p>
-              <div className={styles.icon}>
-                <PhotoIcon />
-              </div>
-              <div className={styles.icon}>
-                <VideoCameraIcon />
-              </div>
-              <div className={styles.icon}>
-                <LinkIcon />
-              </div>
+    <div className={styles.container}>
+      <div className={styles.actionsBar}>
+        <div className={styles.wrapper}>
+          <p className={styles.textContainer}>
+            Auto-save is always on, don&apos;t worry!
+          </p>
+          <div className={styles.insertBar}>
+            <p>Insert: </p>
+            <div className={styles.icon}>
+              <PhotoIcon />
             </div>
-            <div className={styles.buttons}>
-              <button className={styles.saveDraftBtn}>Save Draft</button>
-              <button>Publish</button>
+            <div className={styles.icon}>
+              <VideoCameraIcon />
+            </div>
+            <div className={styles.icon}>
+              <LinkIcon />
             </div>
           </div>
+          <div className={styles.buttons}>
+            <button className={styles.saveDraftBtn}>Save Draft</button>
+            <button>Publish</button>
+          </div>
         </div>
-        <input
-          name="article_title"
-          className={styles.articleTitle}
-          placeholder="Title"
-        />
       </div>
-    </form>
+      <input
+        name="article_title"
+        className={styles.articleTitle}
+        placeholder="Title"
+        autoComplete="off"
+      />
+    </div>
   );
 };
 
