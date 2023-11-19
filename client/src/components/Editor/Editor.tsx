9ff4b50b008211df3aaf9e004e3fd9ecd4fa13de
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-19 11:35:24 -0500

    Component Editor created

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,11 +1,14 @@
 "use client";
-
 import { useEditor, EditorContent } from "@tiptap/react";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import styles from "./Editor.module.scss";
-import { PhotoIcon, VideoCameraIcon, LinkIcon } from "@heroicons/react/24/outline";
+import {
+  PhotoIcon,
+  VideoCameraIcon,
+  LinkIcon,
+} from "@heroicons/react/24/outline";
 
 const ArticleEditor = () => {
   const editor = useEditor({
@@ -19,27 +22,38 @@ const ArticleEditor = () => {
   return (
     <div className={styles.container}>
       <div className={styles.actionsBar}>
-      <div className={styles.wrapper}>
-            <p className={styles.textContainer}>
-              Auto-save is always on, don&apos;t worry!
-            </p>
-            <div className={styles.insertBar}>
-              <p>Insert: </p>
-              <div className={styles.icon}>
-                <PhotoIcon />
-              </div>
-              <div className={styles.icon}>
-                <VideoCameraIcon />
-              </div>
-              <div className={styles.icon}>
-                <LinkIcon />
-              </div>
-            </div>
-            <div className={styles.buttons}><button className={styles.saveDraftBtn}>Save Draft</button>
-            <button>Publish</button></div>
+        <div className={styles.wrapper}>
+          <p className={styles.textContainer}>
+            Auto-save is always on, don&apos;t worry!
+          </p>
+          <div className={styles.formatBar}>
+            <button onClick={() => editor.chain().focus().toggleBold().run()}>
+              Bold
+            </button>
           </div>
+          <div className={styles.insertBar}>
+            <p>Insert: </p>
+            <div className={styles.icon}>
+              <PhotoIcon />
+            </div>
+            <div className={styles.icon}>
+              <VideoCameraIcon />
+            </div>
+            <div className={styles.icon}>
+              <LinkIcon />
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.saveDraftBtn}>Save Draft</button>
+            <button>Publish</button>
+          </div>
+        </div>
       </div>
-      <input name="article_title" className={styles.articleTitle} placeholder="Title"/>
+      <input
+        name="article_title"
+        className={styles.articleTitle}
+        placeholder="Title"
+      />
       <EditorContent className={styles.editorContainer} editor={editor} />
     </div>
   );
