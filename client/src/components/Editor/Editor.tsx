b4328bd3d2e8cf6ba27e44836dc016958ac39f3d
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-22 23:01:18 -0500

    write article page design done

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,7 +1,58 @@
 "use client";
+import {
+  ArrowUpTrayIcon,
+  PhotoIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
+import styles from "./Editor.module.scss";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 const ArticleEditor = () => {
-  return <div>Editor</div>;
+  const [value, setValue] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.actions}>
+        <div>
+          <p>Insert</p>
+          <button className={styles.addButton}>
+            <PhotoIcon width={16} height={16} />
+          </button>
+          <button className={styles.addButton}>
+            <ArrowUpTrayIcon width={16} height={16} />
+          </button>
+          <button className={styles.addButton}>
+            <VideoCameraIcon width={16} height={16} />
+          </button>
+        </div>
+      </div>
+      <input className={styles.title} type="text" placeholder="Title" />
+      <div className={styles.editor}>
+        {/* <button className={styles.button}>
+          <PlusIcon width={16} height={16} />
+        </button>
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <PhotoIcon width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <ArrowUpIcon width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <VideoCameraIcon width={16} height={16} />
+            </button>
+          </div> */}
+        <ReactQuill
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ArticleEditor;
