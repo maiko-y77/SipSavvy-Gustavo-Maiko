b6745f6253e23b00c2d0fa70440b693fa2992430
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-22 21:43:31 -0500

    starting over with the editor

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,45 +1,7 @@
 "use client";
-import styles from "./Editor.module.scss";
-import {
-  PhotoIcon,
-  VideoCameraIcon,
-  LinkIcon,
-} from "@heroicons/react/24/outline";
 
 const ArticleEditor = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.actionsBar}>
-        <div className={styles.wrapper}>
-          <p className={styles.textContainer}>
-            Auto-save is always on, don&apos;t worry!
-          </p>
-          <div className={styles.insertBar}>
-            <p>Insert: </p>
-            <div className={styles.icon}>
-              <PhotoIcon />
-            </div>
-            <div className={styles.icon}>
-              <VideoCameraIcon />
-            </div>
-            <div className={styles.icon}>
-              <LinkIcon />
-            </div>
-          </div>
-          <div className={styles.buttons}>
-            <button className={styles.saveDraftBtn}>Save Draft</button>
-            <button>Publish</button>
-          </div>
-        </div>
-      </div>
-      <input
-        name="article_title"
-        className={styles.articleTitle}
-        placeholder="Title"
-        autoComplete="off"
-      />
-    </div>
-  );
+  return <div>Editor</div>;
 };
 
 export default ArticleEditor;
