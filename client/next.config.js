f37fc7c32d620836325fb864dceae3c62999d994
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-11-27 13:32:45 -0800

    Fixed next config for images

--- client/next.config.js
+++ client/next.config.js
@@ -8,8 +8,6 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
-        ],
-        remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
