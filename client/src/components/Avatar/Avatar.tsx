342452c746267ceec5cea0c1e7d0f6ac0ad9f778
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-11-26 18:28:51 -0800

    Rendered img from server

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -1,10 +1,20 @@
 import { UserIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import "@/components/Avatar/Avatar.scss"
 
-const Avatar = () => {
+type Props = {
+  img?: string
+}
+const Avatar = ({ img }: Props) => {
   return (
-    <a className="avatar icon" href="/">
-      <UserIcon width={24} height={24} />
-    </a>
+    <>
+      {
+        img ? <Image src={img} alt="user avatar" width={36} height={36} /> :
+          <span className="avatar icon" >
+            <UserIcon width={24} height={24} />
+          </span>
+      }
+    </>
   );
 };
 
