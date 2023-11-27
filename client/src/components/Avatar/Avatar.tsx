2b72938f88864f0a008605ecaffaa628c2d9d2a4
Maiko Yorino <mk0510mk@hotmail.co.jp>
2023-11-27 15:08:27 -0800

    Added proparty className of Avatar

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 type Props = {
   img?: string
+  className?: string
 }
-const Avatar = ({ img }: Props) => {
+const Avatar = ({ img, className }: Props) => {
   return (
     <>
       {
-        img ? <Image src={img} alt="user avatar" width={36} height={36} /> :
+        img ? <Image className={className} src={img} alt="user avatar" width={36} height={36} /> :
           <span className="avatar icon" >
             <UserIcon width={24} height={24} />
           </span>
