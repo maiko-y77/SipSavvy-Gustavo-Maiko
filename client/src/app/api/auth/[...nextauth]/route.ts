a2e72a292336293bf4df02a2787276fb3ee41975
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-28 13:08:23 -0800

    Navbar.tsx and route.ts updated still missing the callback function in the authenticate function

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { useRouter } from "next/router";
 
 const handler = NextAuth({
   providers: [
