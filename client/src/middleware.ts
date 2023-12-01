689881a027b35d0fee143ecc0ade0890c2979014
Gustavo <gustavoneripeixoto@gmail.com>
2023-11-30 17:04:31 -0800

    what's been done: 1. Admin route is secure based the user role 2. When the user log in, it redirects to where the user left off, not the standard feed page

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,7 +1,40 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = { matcher: [
+export default withAuth(
+  function middleware(req) {
+    if (
+      req.nextUrl.pathname.startsWith("/admin") &&
+      req.nextauth.token?.role !== "admin"
+    )
+      return NextResponse.redirect(new URL("/feed", req.url));
+  },
+  {
+    callbacks: {
+      authorized: ({ token }: any) => !!token,
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    "/admin/:path*",
     "/feed",
+    "/first-login",
+    "/articles/:path*",
+    "/messages",
+    "/my-collections",
+    "/collection",
+    "/writer",
+    "/articles",
+    "/dashboard",
+    "/users",
+    "/writer/[id]",
+  ],
+};
+
+/* 
+"/feed",
     "/first-login",
     "/articles/[id]",
     "/messages",
@@ -11,5 +44,6 @@ export const config = { matcher: [
     "/articles",
     "/dashboard",
     "/users",
-    "/writer/[id]"
-]} 
+    "/writer/[id]",
+    "/admin/:path*"
+*/
