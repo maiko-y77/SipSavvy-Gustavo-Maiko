f7d47d275200d43a4276c18c1c28a697fd9e3c72
Maiko Yorino <mk0510@mk@hotmail.co.jp>
2023-12-06 04:44:01 +0900

    Updated for uploading cover image

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -5,27 +5,54 @@ import {
   VideoCameraIcon,
 } from "@heroicons/react/24/outline";
 import styles from "./Editor.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 
 const ArticleEditor = () => {
   const [value, setValue] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  };
+
+  useEffect(() => {
+    const upload = () => {};
+
+    file && upload;
+  }, [file]);
 
   return (
     <div className={styles.container}>
       <div className={styles.actions}>
         <div>
           <p>Insert</p>
-          <button className={styles.addButton}>
-            <PhotoIcon width={16} height={16} />
-          </button>
-          <button className={styles.addButton}>
-            <ArrowUpTrayIcon width={16} height={16} />
-          </button>
-          <button className={styles.addButton}>
-            <VideoCameraIcon width={16} height={16} />
-          </button>
+          {
+            <div>
+              <input
+                type="file"
+                id="icon"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+              />
+              <button className={styles.addButton}>
+                <label htmlFor="icon">
+                  <PhotoIcon width={16} height={16} />
+                </label>
+              </button>
+              <button className={styles.addButton}>
+                <ArrowUpTrayIcon width={16} height={16} />
+              </button>
+              <button className={styles.addButton}>
+                <VideoCameraIcon width={16} height={16} />
+              </button>
+            </div>
+          }
         </div>
         <div>
           <button className={styles.saveDraftButton}>Save Draft</button>
