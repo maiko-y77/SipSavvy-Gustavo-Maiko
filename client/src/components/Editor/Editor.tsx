383e7ee556374e6384f627324dd66cea05921aa4
Maiko Yorino <mk0510@mk@hotmail.co.jp>
2023-12-06 08:15:26 +0900

    Handling post method

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -8,24 +8,77 @@ import styles from "./Editor.module.scss";
 import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../../../utils/firebase";
+
+const storage = getStorage(app);
 
 const ArticleEditor = () => {
   const [value, setValue] = useState("");
-  const [file, setFile] = useState<File | null>(null);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0];
-
-    if (selectedFile) {
-      setFile(selectedFile);
-    }
-  };
+  const [file, setFile] = useState(null);
+  const [media, setMedia] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
-    const upload = () => {};
+    const upload = () => {
+      const name = new Date().getTime + file.name;
+      const storageRef = ref(storage, name);
 
-    file && upload;
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setMedia(downloadURL);
+          });
+        }
+      );
+    };
+
+    file && upload();
   }, [file]);
+
+  const slugify = (str) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/[\s_-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+  const handleSubmit = async () => {
+    const res = await fetch("http://localhost:3001/articles/", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc: value,
+        img: media,
+        slug: slugify(title),
+      }),
+    });
+
+    console.log(res);
+  };
 
   return (
     <div className={styles.container}>
@@ -37,7 +90,7 @@ const ArticleEditor = () => {
               <input
                 type="file"
                 id="icon"
-                onChange={handleFileChange}
+                onChange={(e) => setFile(e.target.files[0])}
                 style={{ display: "none" }}
               />
               <button className={styles.addButton}>
@@ -56,10 +109,17 @@ const ArticleEditor = () => {
         </div>
         <div>
           <button className={styles.saveDraftButton}>Save Draft</button>
-          <button className={styles.publishButton}>Publish</button>
+          <button className={styles.publishButton} onClick={handleSubmit}>
+            Publish
+          </button>
         </div>
       </div>
-      <input className={styles.title} type="text" placeholder="Title" />
+      <input
+        className={styles.title}
+        type="text"
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <div className={styles.editor}>
         <ReactQuill
           theme="bubble"
