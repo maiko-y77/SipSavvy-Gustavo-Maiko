cbecdcc19e0c8bfa2f43bc50db970057df07a4c4
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-06 15:14:41 -0800

    middleware updated to protect route against users that are not writers

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -8,6 +8,12 @@ export default withAuth(
       req.nextauth.token?.role !== "admin"
     )
       return NextResponse.redirect(new URL("/feed", req.url));
+
+      if (
+        req.nextUrl.pathname.startsWith("/my-articles") &&
+        req.nextauth.token?.role !== "writer"
+      )
+        return NextResponse.redirect(new URL("/feed", req.url));
   },
   {
     callbacks: {
