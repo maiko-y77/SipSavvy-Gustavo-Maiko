515fc4bb4f08a7b9e416523deb3a97d2186ba0b4
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-06 09:52:20 -0800

    my articles scss updated

--- client/src/app/(writer)/my-articles/page.tsx
+++ client/src/app/(writer)/my-articles/page.tsx
@@ -1,26 +1,7 @@
-import Tab from "@/components/Tab/Tab";
-import styles from "./MyArticles.module.scss";
 import MyArticlesItem from "../../../components/MyArticlesItem/MyArticlesItem";
-import Link from "next/link";
 
 const MyArticles = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        
-        <div className="tab-bar">
-          <Tab text="My Articles" isActive={true} />
-          <Tab text="My Drafts" isActive={false} />
-          <Link href="/editor/new" className={styles.newArticleBtn}>New Article</Link>
-        </div>
-
-        <div className={styles.articlesList}>
-          <MyArticlesItem />
-        </div>
-      </div>
-      <div className={styles.sidebar}>Sidebar</div>
-    </div>
-  );
+  return <MyArticlesItem />;
 };
 
 export default MyArticles;
