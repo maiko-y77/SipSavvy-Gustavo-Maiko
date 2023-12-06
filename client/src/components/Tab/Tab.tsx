5beae210c57afb602d6038aa500b09679d142fc7
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-06 10:21:32 -0800

    console.log removed from Tab component

--- client/src/components/Tab/Tab.tsx
+++ client/src/components/Tab/Tab.tsx
@@ -3,8 +3,6 @@ import { usePathname } from "next/navigation";
 
 const Tab = ({ text, path }: { text: string; path: string }) => {
   const pathname = usePathname();
-  console.log(pathname);
-
   return <div className={`tab ${pathname === path ? "active" : ""}`}>{text}</div>;
 };
 
