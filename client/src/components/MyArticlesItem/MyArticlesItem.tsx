9436e83fd84c661cd4da84b5c80e39c7037c2988
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-06 11:03:54 -0800

    published articles showing

--- client/src/components/MyArticlesItem/MyArticlesItem.tsx
+++ client/src/components/MyArticlesItem/MyArticlesItem.tsx
@@ -3,6 +3,7 @@ import { options } from "@/app/api/auth/[...nextauth]/options";
 import styles from "./MyArticlesItem.module.scss";
 import { Article } from "@/lib/Article/types";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 type ArticlesProps = {
   data: Article[];
@@ -11,16 +12,21 @@ type ArticlesProps = {
 const MyArticlesItem = ({ data }: ArticlesProps) => {
   const session = useSession();
   const filteredData: Article[] = data.filter(
-    (article) => article.author.id === session.data?.user.id
+    (article) => article.author.id === session.data?.user.id && article.status === 'published'
   );
 
   return (
     <div className={styles.articlesList}>
       {filteredData.map((article) => (
         <div key={article.id} className={styles.articleItem}>
-          {article.author.name}
+          <div className={styles.title}>
+            {article.title}
+          </div>
         </div>
       ))}
+      {
+        filteredData.length === 0 && <div className={styles.emptyState}><p>You have no published articles yet. </p> <Link href="/editor/new">Start Writing</Link></div>
+      }
     </div>
   );
 };
