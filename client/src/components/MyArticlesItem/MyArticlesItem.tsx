1efe5a92851d776ce2fc40ab41b76009e6362b9e
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-06 10:25:06 -0800

    frame class added to main content area

--- client/src/components/MyArticlesItem/MyArticlesItem.tsx
+++ client/src/components/MyArticlesItem/MyArticlesItem.tsx
@@ -1,9 +1,13 @@
+"use client"
 import './MyArticlesItem.module.scss'
+import { getArticles } from '@/lib/Articles/data'
 
 const MyArticlesItem = () => {
-  return (
-    <div>MyArticlesItem</div>
-  )
+
+  const articles = getArticles();
+  console.log(articles)
+  
+  return <div className='test'>Vamos la</div>
 }
 
 export default MyArticlesItem
