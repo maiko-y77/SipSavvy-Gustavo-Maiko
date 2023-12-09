ce849610a4b3d6181d772aaa0a641f26f93b466e
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-09 13:41:06 -0800

    burger menu working on screens smaller than 1024px

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,45 +1,60 @@
+"use client"
 import { Logo } from "../Logo/Logo";
 import { AdminLogo } from "../Logo/Logo";
-import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
+// import { getServerSession } from "next-auth";
+// import { options } from "@/app/api/auth/[...nextauth]/options";
 import LogoutBtn from "../LogoutBtn/LogoutBtn";
 import Link from "next/link";
 import "./Navbar.scss";
 import Image from "next/image";
 import AdminOptions from "../AdminOptions/AdminOptions";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 
-export default async function Navbar() {
-  const data = await getServerSession(options);
-  const userRole = data?.user.role;
+export default function Navbar() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const { data }  = useSession();
+  const { name, avatar, role } = data?.user || {}
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsNavbarOpen(false);
+  };
 
   return (
-    <header className={userRole === "admin" ? "header admin-header" : "header"}>
-      {userRole === "admin" ? <AdminLogo /> : <Logo />}
+    <header className={role === "admin" ? "header admin-header" : "header"}>
+      {role === "admin" ? <AdminLogo /> : <Logo />}
 
-      <nav className="default_navbar">
-        {userRole === "admin" && <AdminOptions />}
-        <Link href={data ? "/feed" : "/"}>Home</Link>
-        {userRole === "writer" && (
+      <nav className={`default_navbar ${isNavbarOpen ? "open" : ""} mobile-nav`}>
+        <div className="links">
+        {role === "admin" && <AdminOptions />}
+        <Link href={data ? "/feed" : "/"} onClick={closeNavbar}>Home</Link>
+        {role === "writer" && (
           <>
-            <Link href={`/editor/new`}>Write</Link>
-            <Link href={`/writers/${data?.user.id}`}>My Profile</Link>
-            <Link href="/my-articles">My Articles</Link>
+            <Link href={`/editor/new`} onClick={closeNavbar}>Write</Link>
+            <Link href={`/writers/${data?.user.id}`} onClick={closeNavbar}>My Profile</Link>
+            <Link href="/my-articles" onClick={closeNavbar}>My Articles</Link>
           </>
         )}
-        <Link href="/about">About Us</Link>
-        {userRole !== "admin" && <Link href="/contact">Contact Us</Link>}
+        <Link href="/about" onClick={closeNavbar}>About Us</Link>
+        {role !== "admin" && <Link href="/contact" onClick={closeNavbar}>Contact Us</Link>}
+        </div>
         <div className="wrap-icons">
           {data ? (
             <div className="droppeddown-loggedin">
               <Image
                 className="avatar icon"
-                src={data?.user.avatar}
+                src={`${avatar}`}
                 width={24}
                 height={24}
-                alt={data?.user.avatar + `'s Avatar`}
+                alt={name + `'s Picture`}
               />
               <p>
-                Hello, <span>{data?.user.name}</span>!
+                Hello, <span>{name}</span>!
               </p>
             </div>
           ) : (
@@ -49,14 +64,19 @@ export default async function Navbar() {
             <LogoutBtn />
           ) : (
             <>
-              <Link href="/login">Login</Link>
-              <Link href="/register" className="primary-btn">
+              <Link href="/login" onClick={closeNavbar}>Login</Link>
+              <Link href="/register" className="primary-btn" onClick={closeNavbar}>
                 Register
               </Link>
             </>
           )}
         </div>
       </nav>
+
+      <Link href="" className="burger" onClick={toggleNavbar}>
+        <Bars3Icon width={36} height={24} />
+      </Link>
+
     </header>
   );
 }
