fd3af7c671a508bfd59590f7a64e27341f830a2f
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-08 17:31:06 -0800

    starting over'

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import "./Header.scss";
+import { Logo, AdminLogo } from "../Logo/Logo";
+import { getServerSession } from "next-auth/next";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import Navbar from "../Navbar/Navbar";
+import { Bars2Icon } from "@heroicons/react/24/outline";
+
+export default async function Header() {
+
+  const data = await getServerSession(options)
+  const userRole = data?.user.role
+
+  return (
+    <header className={userRole === "admin" ? "admin-header" : "header"}>
+      <div className="logo">
+        {userRole === "admin" ? <AdminLogo /> : <Logo />}
+      </div>
+
+      <Navbar data={data} />
+
+      <div className="burger">
+        <Bars2Icon width={24} height={24} />
+      </div>
+
+    </header>
+  );
+}
