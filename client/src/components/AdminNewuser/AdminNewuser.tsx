e34f6802093eb534b1bd42815295b9b1942a5e32
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-09 10:41:18 -0800

    admin dashboard css fixed

--- client/src/components/AdminNewuser/AdminNewuser.tsx
+++ client/src/components/AdminNewuser/AdminNewuser.tsx
@@ -3,26 +3,24 @@ import Link from "next/link";
 import { User } from "@/lib/User/types";
 import Avatar from "../Avatar/Avatar";
 
-type USersProps = {
+type UsersProps = {
   data: User[];
 };
 
 const BASE_CLASS = "new-user";
 
-export default async function AdminNewuser({ data }: USersProps) {
+export default async function AdminNewuser({ data }: UsersProps) {
   return (
     <>
       {data.map(({ id, avatar, name, last_name }) => (
-        <div key={id}>
-          <div className={`${BASE_CLASS}`}>
-            <Link className="avatar icon" href="/">
-              <Avatar className="avatar" img={String(avatar)} />
+        <div className={`${BASE_CLASS}`} key={id}>
+          <Link className="avatar icon" href="/">
+            <Avatar className="avatar" img={String(avatar)} />
+          </Link>
+          <div className={`${BASE_CLASS}__name`}>
+            <Link href="/admin/users/" className="name">
+              {name} {last_name}
             </Link>
-            <div className={`${BASE_CLASS}__name`}>
-              <p className="name">
-                {name} {last_name}
-              </p>
-            </div>
           </div>
         </div>
       ))}
