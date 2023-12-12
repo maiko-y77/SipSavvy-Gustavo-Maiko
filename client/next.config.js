506ee02a6f2f5685fc4bd6efc5f7ae7093a176dd
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-12 13:08:37 -0800

    next.config adjusted

--- client/next.config.js
+++ client/next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'gustavoneri.art',
+                port: '',
+                pathname: '/**',
+            }
         ],
     },
 }
