303102d050b2a9ca030f2d4c7ab73e65bd8c23ab
Gustavo <gustavoneripeixoto@gmail.com>
2023-12-12 13:20:53 -0800

    logout redirecting to index page now

--- client/src/components/LogoutBtn/LogoutBtn.tsx
+++ client/src/components/LogoutBtn/LogoutBtn.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const LoginLogoutBtn = () => {
   return (
-    <Link href="" onClick={()=>signOut()}>
+    <Link href="" onClick={()=>signOut({ callbackUrl: '/' })}>
       <PowerIcon width={22} height={22} />
     </Link>
   );
